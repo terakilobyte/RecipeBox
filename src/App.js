781e9652df51af6b2fb25c9181e37ed71f58458c
DanStockham <ds321609@gmail.com>
2016-06-28 19:58:10 -0400

    Added puesdo code for guidance

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ require('./main.scss');
 import React from 'react';
 import {render} from 'react-dom';
 
+//How should recipe data be stored?
+
+//A component that renders different recipes
+  //recipes are rendered seperately
+    //A button that allows the user to delete a recipe
+    //A button that allows the user to edit the recipe
+  //A button that allows the user to create a new recipe
+
+//A component that allows for recipe management
+  //If the edit button is clicked, the recipe is populated with recipe name and ingredents. The ingredents are seperated by commas
+    //The user can add additional ingredents as long there is a comma seperated by the rest of the data
+    //The user can change the name of the recipe
+  //If add button is pressed, then a blank form is presented to the user to add a new recipe
+    //If no recipe name is added, the recipe name will be 'Untitled'
+    
+
 
 
 //React compenents and render
@@ -13,7 +29,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      userData: null
+      //store an instances of local storage
+
     }
   }
 
